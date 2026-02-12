7ff04f8e949ad91e942fbf9fb8d7818e35115726
yschen20 <ysc19826085315@qq.com>
2026-02-12 17:51:22 +0800

    Updated: 2026-02-12 17:51:22

--- Java题目/MoeCTF-2025-23 第二十三章 幻境迷心·皇陨星沉(大结局)/MoeCTF-2025-23/src/main/java/com/example/demo/Dog/Dog.java
+++ Java题目/MoeCTF-2025-23 第二十三章 幻境迷心·皇陨星沉(大结局)/MoeCTF-2025-23/src/main/java/com/example/demo/Dog/Dog.java
@@ -1,57 +1,48 @@
-/*    */ package com.example.demo.Dog;
-/*    */ import com.example.demo.Dog.DogModel;
-/*    */ import java.io.Serializable;
-/*    */ import java.util.Objects;
-/*    */ 
-/*    */ public class Dog implements Serializable, DogModel {
-/*    */   private int id;
-/*    */   private String name;
-/*    */   private String breed;
-/*    */   private int age;
-/*    */   private int hunger;
-/*    */   Object object;
-/*    */   String methodName;
-/*    */   Class[] paramTypes;
-/*    */   Object[] args;
-/*    */   
-/*    */   public Dog(int id, String name, String breed, int age) {
-/* 18 */     this.id = id;
-/* 19 */     this.name = name;
-/* 20 */     this.breed = breed;
-/* 21 */     this.age = age;
-/* 22 */     this.hunger = 50;
-/*    */   }
-/*    */   
-/*    */   public int getId() {
-/* 26 */     return this.id;
-/* 27 */   } public void setId(int id) { this.id = id; }
-/* 28 */   public String getName() { return this.name; }
-/* 29 */   public String getBreed() { return this.breed; }
-/* 30 */   public int getAge() { return this.age; } public int getHunger() {
-/* 31 */     return this.hunger;
-/*    */   }
-/*    */   public void feed() {
-/* 34 */     this.hunger = Math.max(0, this.hunger - 10);
-/*    */   }
-/*    */ 
-/*    */ 
-/*    */   
-/*    */   public boolean equals(Object o) {
-/* 40 */     if (this == o) return true; 
-/* 41 */     if (o == null || getClass() != o.getClass()) return false; 
-/* 42 */     com.example.demo.Dog.Dog dog = (com.example.demo.Dog.Dog)o;
-/* 43 */     return (this.id == dog.id);
-/*    */   }
-/*    */ 
-/*    */   
-/*    */   public int hashCode() {
-/* 48 */     wagTail(this.object, this.methodName, this.paramTypes, this.args);
-/* 49 */     return Objects.hash(new Object[] { Integer.valueOf(this.id) });
-/*    */   }
-/*    */ }
+package com.example.demo.Dog;
+import com.example.demo.Dog.DogModel;
+import java.io.Serializable;
+import java.util.Objects;
 
+public class Dog implements Serializable, DogModel {
+    private int id;
+    private String name;
+    private String breed;
+    private int age;
+    private int hunger;
+    Object object;
+    String methodName;
+    Class[] paramTypes;
+    Object[] args;
 
-/* Location:              E:\CTF\网络安全学习笔记\漏洞\Java安全\题目\MoeCTF-2025-23 第二十三章 幻境迷心·皇陨星沉(大结局)\demo.jar!\BOOT-INF\classes\com\example\demo\Dog\Dog.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
+    public Dog(int id, String name, String breed, int age) {
+        this.id = id;
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.hunger = 50;
+    }
+
+    public int getId() {
+        return this.id;
+    } public void setId(int id) { this.id = id; }
+    public String getName() { return this.name; }
+    public String getBreed() { return this.breed; }
+    public int getAge() { return this.age; } public int getHunger() {
+        return this.hunger;
+    }
+    public void feed() {
+        this.hunger = Math.max(0, this.hunger - 10);
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        com.example.demo.Dog.Dog dog = (com.example.demo.Dog.Dog)o;
+        return (this.id == dog.id);
+    }
+
+    public int hashCode() {
+        wagTail(this.object, this.methodName, this.paramTypes, this.args);
+        return Objects.hash(new Object[] { Integer.valueOf(this.id) });
+    }
+}
--- Java题目/MoeCTF-2025-23 第二十三章 幻境迷心·皇陨星沉(大结局)/MoeCTF-2025-23/src/main/java/com/example/demo/Dog/DogService.java
+++ Java题目/MoeCTF-2025-23 第二十三章 幻境迷心·皇陨星沉(大结局)/MoeCTF-2025-23/src/main/java/com/example/demo/Dog/DogService.java
@@ -1,86 +1,70 @@
-/*    */ package com.example.demo.Dog;
-/*    */ import com.example.demo.Dog.Dog;
+package com.example.demo.Dog;
+import com.example.demo.Dog.Dog;
 import org.springframework.stereotype.Service;
-/*    */ import java.io.*;
-/*    */
-/*    */
-/*    */
-/*    */ import java.util.*;
+import java.io.*;
 
-/*    */
-/*    */ @Service
-/*    */ public class DogService implements Serializable {
-/* 11 */   private Map<Integer, Dog> dogs = new HashMap<>();
-/* 12 */   private int nextId = 1;
-/*    */ 
-/*    */   
-/*    */   public List<Dog> getAllDogs() {
-/* 16 */     return new ArrayList<>(this.dogs.values());
-/*    */   }
-/*    */ 
-/*    */   
-/*    */   public Dog addDog(String name, String breed, int age) {
-/* 21 */     Dog dog = new Dog(this.nextId++, name, breed, age);
-/* 22 */     this.dogs.put(Integer.valueOf(dog.getId()), dog);
-/* 23 */     return dog;
-/*    */   }
-/*    */ 
-/*    */   
-/*    */   public Dog feedDog(int id) {
-/* 28 */     Dog dog = this.dogs.get(Integer.valueOf(id));
-/* 29 */     if (dog != null) {
-/* 30 */       dog.feed();
-/*    */     }
-/* 32 */     return dog;
-/*    */   }
-/*    */ 
-/*    */   
-/*    */   public Dog removeDog(int id) {
-/* 37 */     return this.dogs.remove(Integer.valueOf(id));
-/*    */   }
-/*    */   public Object chainWagTail() {
-/* 40 */     Object input = null;
-/* 41 */     for (Dog dog : this.dogs.values()) {
-/* 42 */       if (input == null) {
-/* 43 */         input = dog.object;
-/*    */       }
-/* 45 */       Object result = dog.wagTail(input, dog.methodName, dog.paramTypes, dog.args);
-/* 46 */       input = result;
-/*    */     } 
-/* 48 */     return input;
-/*    */   }
-/*    */ 
-/*    */   
-/*    */   public String exportDogsBase64() {
-/* 53 */     try(ByteArrayOutputStream baos = new ByteArrayOutputStream(); 
-/* 54 */         ObjectOutputStream oos = new ObjectOutputStream(baos)) {
-/* 55 */       oos.writeObject(new ArrayList(this.dogs.values()));
-/* 56 */       oos.flush();
-/* 57 */       return Base64.getEncoder().encodeToString(baos.toByteArray());
-/* 58 */     } catch (IOException e) {
-/* 59 */       e.printStackTrace();
-/* 60 */       return "";
-/*    */     } 
-/*    */   }
-/*    */ 
-/*    */ 
-/*    */   
-/*    */   public void importDogsBase64(String base64Data) {
-/* 67 */     try(ByteArrayInputStream bais = new ByteArrayInputStream(Base64.getDecoder().decode(base64Data)); 
-/* 68 */         ObjectInputStream ois = new ObjectInputStream(bais)) {
-/* 69 */       Collection<Dog> imported = (Collection<Dog>)ois.readObject();
-/* 70 */       for (Dog dog : imported) {
-/* 71 */         dog.setId(this.nextId++);
-/* 72 */         this.dogs.put(Integer.valueOf(dog.getId()), dog);
-/*    */       } 
-/* 74 */     } catch (IOException|ClassNotFoundException e) {
-/* 75 */       e.printStackTrace();
-/*    */     } 
-/*    */   }
-/*    */ }
+import java.util.*;
 
+@Service
+public class DogService implements Serializable {
+    private Map<Integer, Dog> dogs = new HashMap<>();
+    private int nextId = 1;
 
-/* Location:              E:\CTF\网络安全学习笔记\漏洞\Java安全\题目\MoeCTF-2025-23 第二十三章 幻境迷心·皇陨星沉(大结局)\demo.jar!\BOOT-INF\classes\com\example\demo\Dog\DogService.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
+    public List<Dog> getAllDogs() {
+        return new ArrayList<>(this.dogs.values());
+    }
+
+    public Dog addDog(String name, String breed, int age) {
+        Dog dog = new Dog(this.nextId++, name, breed, age);
+        this.dogs.put(Integer.valueOf(dog.getId()), dog);
+        return dog;
+    }
+
+    public Dog feedDog(int id) {
+        Dog dog = this.dogs.get(Integer.valueOf(id));
+        if (dog != null) {
+            dog.feed();
+        }
+        return dog;
+    }
+
+    public Dog removeDog(int id) {
+        return this.dogs.remove(Integer.valueOf(id));
+    }
+    public Object chainWagTail() {
+        Object input = null;
+        for (Dog dog : this.dogs.values()) {
+            if (input == null) {
+                input = dog.object;
+            }
+            Object result = dog.wagTail(input, dog.methodName, dog.paramTypes, dog.args);
+            input = result;
+        }
+        return input;
+    }
+
+    public String exportDogsBase64() {
+        try(ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(baos)) {
+            oos.writeObject(new ArrayList(this.dogs.values()));
+            oos.flush();
+            return Base64.getEncoder().encodeToString(baos.toByteArray());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    public void importDogsBase64(String base64Data) {
+        try(ByteArrayInputStream bais = new ByteArrayInputStream(Base64.getDecoder().decode(base64Data));
+            ObjectInputStream ois = new ObjectInputStream(bais)) {
+            Collection<Dog> imported = (Collection<Dog>)ois.readObject();
+            for (Dog dog : imported) {
+                dog.setId(this.nextId++);
+                this.dogs.put(Integer.valueOf(dog.getId()), dog);
+            }
+        } catch (IOException|ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
